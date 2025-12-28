022395388d83eb63a2af448b8a6228b786397108
kiran <kiran@technource.in>
2025-12-28 22:55:17 +0530

    - navbar update.

--- resources/views/layouts/sections/navbar/navbar-partial.blade.php
+++ resources/views/layouts/sections/navbar/navbar-partial.blade.php
@@ -454,7 +454,7 @@ use Illuminate\Support\Facades\Route;
       <ul class="dropdown-menu dropdown-menu-end">
         <li>
           <a class="dropdown-item mt-0"
-            href="#">
+            href="{{ route('profile') }}">
             <div class="d-flex align-items-center">
               <div class="flex-shrink-0 me-2">
                 <div class="avatar avatar-online">
@@ -480,8 +480,13 @@ use Illuminate\Support\Facades\Route;
         </li>
         <li>
           <a class="dropdown-item"
-            href="#">
+            href="{{ route('profile') }}">
             <i class="icon-base ti tabler-user me-3 icon-md"></i><span class="align-middle">My Profile</span> </a>
+        </li>
+        <li>
+          <a class="dropdown-item"
+            href="{{ route('security') }}">
+            <i class="icon-base ti tabler-settings me-3 icon-md"></i><span class="align-middle">Setting</span> </a>
         </li>
         @if (Auth::check() && Laravel\Jetstream\Jetstream::hasApiFeatures())
         <li>
@@ -489,15 +494,6 @@ use Illuminate\Support\Facades\Route;
             <i class="icon-base ti tabler-settings me-3 icon-md"></i><span class="align-middle">API Tokens</span> </a>
         </li>
         @endif
-        <li>
-          <a class="dropdown-item" href="#">
-            <span class="d-flex align-items-center align-middle">
-              <i class="flex-shrink-0 icon-base ti tabler-file-dollar me-3 icon-md"></i><span
-                class="flex-grow-1 align-middle">Billing</span>
-              <span class="flex-shrink-0 badge bg-danger d-flex align-items-center justify-content-center">4</span>
-            </span>
-          </a>
-        </li>
         @if (Auth::User() && Laravel\Jetstream\Jetstream::hasTeamFeatures())
         <li>
           <div class="dropdown-divider my-1 mx-n2"></div>
